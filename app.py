a48a1134ec8d7d5bd967959365baa09d214fb142
Denny <denny12079@gmail.com>
2019-08-07 21:10:12 +0800

    1

--- app.py
+++ app.py
@@ -31,17 +31,17 @@ def callback():
     return 'OK'
 
 # 處理訊息
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # get user id when reply
     user_id = event.source.user_id
     print("user_id =", user_id)
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-
+    
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
 
 import os
 if __name__ == "__main__":
