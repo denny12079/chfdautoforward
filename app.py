42680d9d25e876005cd81823c08e924bdc833cb2
Denny <denny12079@gmail.com>
2019-08-08 11:07:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,11 +31,17 @@ def callback():
 
     return 'OK'
 
+'@handler.add(MessageEvent, message=TextMessage)
+'def handle_message(event):
+    'line_bot_api.reply_message(
+       ' event.reply_token,
+        'TextSendMessage(text=event.message.text))http://localhost:8888/edit/Documents/GitHub/chfdautoforward/app.py#
+    
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    # get user id when reply
+    user_id = event.source.user_id
+    print("user_id =", user_id)
 
 
 if __name__ == "__main__":
