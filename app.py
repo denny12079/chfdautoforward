55a3a5f2a09a86152ba558c7e3d45c64633f6809
Denny <denny12079@gmail.com>
2019-08-08 11:15:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))http://09333298.ngrok.io/edit/Documents/GitHub/chfdautoforward/app.py#
+        TextSendMessage(text=event.message.text))https://09333298.ngrok.io/edit/Documents/GitHub/chfdautoforward/app.py#
  
 
 
