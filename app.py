193492474f1f76b0f4e53d4e19cd98b28773c7b2
Denny <denny12079@gmail.com>
2019-08-08 12:20:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def handle_join(event):
             event.reply_token,
             TextMessage(text=newcoming_text)
 
-import os
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
